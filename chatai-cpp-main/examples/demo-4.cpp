99fcd34fd3ed73c8e4cf025bffb94d7b9d1f1ce8
WinterShadowy <160998818+WinterShadowy@users.noreply.github.com>
2025-03-18 14:49:48 +0800

    Update demo-4.cpp

--- chatai-cpp-main/examples/demo-4.cpp
+++ chatai-cpp-main/examples/demo-4.cpp
@@ -3,8 +3,8 @@
 #include <iostream>
 #include <vector>
 
-std::string api_key = "hk-p9q8f71000035387f6bf91e3e3a4b36a6a9c1b0fcd7edbdc";
-std::string url = "https://api.openai-hk.com/v1/";
+std::string api_key = "YOUR_API_KEY";
+std::string url = "YOUR_URL";
 std::string model = "gpt-4o-mini-2024-07-18"; // gpt-3.5-turbo...... 
 using namespace std;
 
